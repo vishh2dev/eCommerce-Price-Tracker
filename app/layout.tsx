ce24962d062e318c24fea5c29ed1137eccfe5554
vishh2dev <vishh2dev@gmail.com>
2023-10-17 23:09:35 +0530

    feat:added navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter,Space_Grotesk } from 'next/font/google'
 import './globals.css'
+import Navbar from '@/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 const spaceGrotesk = Space_Grotesk({ subsets: ['latin'] , weight:['300','400','500','600','700'] })
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <main className='max-w-10xl mx-auto'>
+          <Navbar/>
           {children}
         </main>
         </body>
