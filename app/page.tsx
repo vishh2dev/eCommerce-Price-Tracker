386baa2e23ae85a39532af02c08dbc90224c7967
vishh2dev <vishh2dev@gmail.com>
2023-10-18 23:55:40 +0530

    feat:creating homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,33 @@
+import Image from 'next/image'
 import React from 'react'
 
-const page = () => {
+const Home = () => {
   return (
-    <div>
-      Pages
-    </div>
+    <>
+    <section className='px-6 md:px-20 py-24 border-2 border-red-500'>
+      <div className='flex max-xl:flex-col gap-16'>
+        <div className='flex flex-col justify-center'>
+          <p className='small-text'>
+            Smart Shopping Starts Here
+            <Image
+              src='/assets/icons/arrow-right.svg'
+              alt='arrow-right'
+              width={16}
+              height={16}
+            />
+          </p>
+          <h1 className='head-text'>
+            Unleashing the power of
+            <span className='text-primary'>PriceTracker</span>
+          </h1>
+          <p className='mt-6'>
+            Powerful, self-serve product and growth analytics to help you convert, engage, and retain more.
+          </p>
+        </div>
+      </div>
+    </section>
+    </>
   )
 }
 
-export default page
+export default Home
