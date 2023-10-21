fc3c7bbee86b8500656f5605b2d6d3bb3792a1ea
vishh2dev <vishh2dev@gmail.com>
2023-10-21 23:37:16 +0530

    feat:searchbar changes

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -4,9 +4,39 @@ import { FormEvent, useState } from "react"
 
 const Searchbar = () => {
     const[ searchPrompt,setSearchPrompt] = useState('')
+    const[ isLoading,setIsLoading] = useState(false)
+
+    const isValidProductUrl = (url: string) =>{
+        try {
+            const parsedUrl = new URL(url)
+            const hostname = parsedUrl.hostname
+
+            if(hostname.includes('amazon.com') ||
+                hostname.includes('amazon.') ||
+                hostname.endsWith('amazon')
+             ){
+                return true
+             }
+        } catch (error) {
+            return false
+        }
+        return false
+    }
 
     const handleSubmit = (event: FormEvent<HTMLFormElement>) =>{
         event.preventDefault()
+
+        const isValidLink = isValidProductUrl(searchPrompt)
+
+        if(!isValidLink) return alert('Please provide a valid amazon link')
+
+        try {
+            setIsLoading(true)
+        } catch (error) {
+            console.log(error)
+        }finally{
+            setIsLoading(false)
+        }
     }
     return (
         <form className='flex flex-wrap gap-4 mt-12' onSubmit={handleSubmit}>
@@ -17,7 +47,9 @@ const Searchbar = () => {
                 placeholder="Enter product link" 
                 className="searchbar-input"
             />
-            <button type="submit" className="searchbar-btn">Search</button>
+            <button  type="submit" className="searchbar-btn" disabled={searchPrompt === ''}>
+                {isLoading ? 'searching..' : 'search'}
+            </button>
         </form>
     )
 }
